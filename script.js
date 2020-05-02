6b0a42813e61df1cd6526ad206874acbc91767ae
Leonid <example@gmail.com>
2020-05-02 22:43:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use srict';
+
+let person = {
+    name: "Jone",
+    age: 25,
+    isMerried: false
+};
+
+console.log(person.name); 
+
+    let arr = ['plum.png','orange.jpg','apple.bmp'];
+    console.log(arr[2]);
+
+    // alert('hello world!')
+
+//    let answer = confirm('Are you here?');
+
+//    console.log(answer);
+
+    // let answer = +prompt('Есть ли Вам 18', "Да");
+
+    //     console.log(typeof(answer));
+
+    // console.log("arr" + " - object");
+
+    let incr = 10,
+        decr = 10;
+
+
+    console.log(incr++);
+    console.log(decr--);
+
+     console.log(5%2);
+    console.log("2" === 2);
+
+    let isChecked = true,
+        isClose = true;
+
+        console.log(isChecked || isClose );
+
+
+
+
